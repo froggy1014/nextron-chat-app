23edf997bac944541295454851f431230b903506
froggy1014 <clcl6084@gmail.com>
2023-01-20 09:48:58 +0900

    feat: create ProtectedRoute Logic

--- renderer/components/Navbar.tsx
+++ renderer/components/Navbar.tsx
@@ -1,22 +1,38 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
+import { useAuth } from "../context/AuthContext";
 
 function Navbar() {
+  const { user, logout } = useAuth()
+  const router = useRouter();
   return (
     <div className="flex justify-around">
       <Link href="/home">
         <a>홈</a>
       </Link>
-      <Link href="/dashboard">
-        <a>대시보드</a>
-      </Link>
-      <Link href="/login">
-        <a>로그인</a>
-      </Link>
-      <Link href="/signup">
-        <a>회원가입</a>
-      </Link>
+      { user ? (
+        <>
+          <Link href="/dashboard">
+            <a>대시보드</a>
+          </Link>
+          <div onClick={() => {
+            logout()
+            router.push('/login');
+          }}>
+          로그아웃
+          </div>
+        </>
+      ) : (
+      <>
+        <Link href="/login">
+          <a>로그인</a>
+        </Link>
+        <Link href="/signup">
+          <a>회원가입</a>
+        </Link>
+      </>
+      )}
     </div>
   );
 }
--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -5,13 +5,22 @@ import '../styles/index.css';
 import Navbar from '../components/Navbar';
 import { AuthContextProvider } from '../context/AuthContext';
 import { useRouter } from 'next/router';
+import ProtectedRoute from '../components/ProtectedRoute';
 
+const noAuthRequired = ['/', '/login', '/signup'];
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <AuthContextProvider>
       <Navbar />
-      <Component {...pageProps} />
+      {noAuthRequired.includes(router.pathname) ? (
+        <Component {...pageProps} />
+      ): (
+        <ProtectedRoute>
+          <Component {...pageProps} />
+        </ProtectedRoute>
+      )}
     </AuthContextProvider>
   )
 }
